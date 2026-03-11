cd54ecec49fec8b5c2e0459252840c5421d61c63
bhushanlamba <bhushan.lamba@ciphersquare.tech>
2026-03-11 17:50:08 +0530

    First Commit from Bhushan

--- app/src/main/java/it/softbrain/barcomall/presentation/ui/products/ProductDetailsFragment.kt
+++ app/src/main/java/it/softbrain/barcomall/presentation/ui/products/ProductDetailsFragment.kt
@@ -257,6 +257,8 @@ class ProductDetailsFragment : Fragment() {
         }
     }
 
+    //Bhushan Branch
+
     private fun setUpDetailWebView(description: String) {
         val htmlStringFromApi =
             """<html><body>$description</body></html>"""  // wrap it in full HTML
